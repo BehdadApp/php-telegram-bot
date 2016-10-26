66211eba7400f66b17cb5c7978c4f07a285e2cd6
Mohammad Behdad <m.behdad7@gmail.com>
2016-10-26 16:42:13 +0330

    Update index.php

--- index.php
+++ index.php
@@ -18,9 +18,13 @@ $string = json_decode(file_get_contents('php://input'));
     $user_id = $result['message']['from']['id'];
     $text = $result['message']['text'];
     $token = '282292401:AAE2ns2v0dHgEopS58BhllzKh1694mDGavs';
-    $text_reply ='dddddddddddddddddddddddddddddddddddddd';
+    $text_reply ='<b>bold</b>, <strong>bold</strong>
+<i>italic</i>, <em>italic</em>
+<a href="http://www.example.com/">inline URL</a>
+<code>inline fixed-width code</code>
+<pre>pre-formatted fixed-width code block</pre>';
     
-    $url = 'https://api.telegram.org/bot'.$token.'/sendMessage?parse_mode=Markdown?chat_id='.$user_id;
+    $url = 'https://api.telegram.org/bot'.$token.'/sendMessage?parse_mode=html&chat_id='.$user_id;
     $url .= '&text=' .$text_reply;
     
         $res = file_get_contents($url);
